2ff5ed2f8d9f56c2c736cf0e5c8d08f2557939d1
Lyra007 <elfishing07@gmail.com>
2018-08-20 19:58:21 +0800

    add log

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var app = express();
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/middle.html');
     //change google.com to the website host pdf file
-    res.redirect(302, 'http://www.google.com');
 });
 
 app.listen(3000, function () {
--- ws.js
+++ ws.js
@@ -2,5 +2,9 @@ var WebSocketServer = require('ws').Server,
   wss = new WebSocketServer({port: 40510})
 
 wss.on('connection', function (ws) {
-  ws.send('test');
-})
+  ws.send(`${new Date()}`);
+  ws.on('message', function (message) {
+    console.log('received: %s', message);
+  });
+
+});
